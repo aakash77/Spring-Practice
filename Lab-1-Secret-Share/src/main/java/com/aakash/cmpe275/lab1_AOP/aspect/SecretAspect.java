81a76d8f042e072783bacf2d85a082ad635f5ead
Aakash Mangal <aakash.mangal77@gmail.com>
2015-10-03 01:05:37 -0700

    added before junit

--- Lab-1-Secret-Share/src/main/java/com/aakash/cmpe275/lab1_AOP/aspect/SecretAspect.java
+++ Lab-1-Secret-Share/src/main/java/com/aakash/cmpe275/lab1_AOP/aspect/SecretAspect.java
@@ -1,5 +1,7 @@
 package com.aakash.cmpe275.lab1_AOP.aspect;
 
+import java.util.UUID;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -27,14 +29,76 @@ public class SecretAspect {
 				throw new NullPointerException();
 			Secret secret = (Secret) methodArgs[1];
 			String key = userId+"#"+secret.getUUID();
-			if(App.userSecrets.contains(key))
+			if(App.userSecrets.containsKey(key))
 				throw new SecretException("Secret Exists");
 		}catch(NullPointerException e){
 			throw new SecretException("Input cannot be null"); 
 		}
 	}
 	
+	/**
+	 * Advice for validating and authorizing request for reading secret
+	 * @param joinPoint
+	 * @throws SecretException
+	 */
+	@Before("execution(* com.aakash.cmpe275.lab1_AOP.service.SecretService.readSecret(..))")
+	public void readSecretAdvice(JoinPoint joinPoint) throws SecretException{
+		Object methodArgs[] = joinPoint.getArgs();
+		try{
+			String userId = (String) methodArgs[0];
+			UUID secretId = (UUID) methodArgs[1];
+			if(userId==null || userId=="" || secretId==null)
+				throw new NullPointerException();
+			String key = userId+"#"+secretId;
+			if(!App.userSecrets.containsKey(key) || !App.sharedSecrets.containsKey(key))
+				throw new SecretException("Unauthorized Exception");
+		}catch(NullPointerException e){
+			throw new SecretException("Input cannot be null"); 
+		}
+	}
 	
+	/**
+	 * Advice for validating and authorizing request of sharing advice 
+	 * @param joinPoint
+	 * @throws SecretException
+	 */
+	@Before("execution(* com.aakash.cmpe275.lab1_AOP.service.SecretService.shareSecret(..))")
+	public void shareSecretAdvice(JoinPoint joinPoint) throws SecretException{
+		Object methodArgs[] = joinPoint.getArgs();
+		try{
+			String userId = (String) methodArgs[0];
+			UUID secretId = (UUID) methodArgs[1];
+			String targetUserId = (String) methodArgs[2];
+			if(userId==null || userId=="" || secretId==null || targetUserId==null || targetUserId=="")
+				throw new NullPointerException();
+			String key = userId+"#"+secretId;
+			if(!App.userSecrets.containsKey(key) || !App.sharedSecrets.containsKey(key))
+				throw new SecretException("Unauthorized Exception");
+		}catch(NullPointerException e){
+			throw new SecretException("Input cannot be null"); 
+		}
+	}
 	
+	/**
+	 * Advice for validating and authorizing request for unsharing the secret
+	 * @param joinPoint
+	 * @throws SecretException
+	 */
+	@Before("execution(* com.aakash.cmpe275.lab1_AOP.service.SecretService.unshareSecret(..))")
+	public void unshareSecretAdvice(JoinPoint joinPoint) throws SecretException{
+		Object methodArgs[] = joinPoint.getArgs();
+		try{
+			String userId = (String) methodArgs[0];
+			UUID secretId = (UUID) methodArgs[1];
+			String targetUserId = (String) methodArgs[2];
+			if(userId==null || userId=="" || secretId==null || targetUserId==null || targetUserId=="")
+				throw new NullPointerException();
+			String key = userId+"#"+secretId;
+			if(!App.userSecrets.containsKey(key) || !App.sharedSecrets.containsKey(key))
+				throw new SecretException("Unauthorized Exception");
+		}catch(NullPointerException e){
+			throw new SecretException("Input cannot be null"); 
+		}
+	}
 	
 }
--- Lab-1-Secret-Share/src/main/java/com/aakash/cmpe275/lab1_AOP/service/impl/SecretServiceImpl.java
+++ Lab-1-Secret-Share/src/main/java/com/aakash/cmpe275/lab1_AOP/service/impl/SecretServiceImpl.java
@@ -11,27 +11,62 @@ import com.aakash.cmpe275.lab1_AOP.service.SecretService;
 @Component
 public class SecretServiceImpl implements SecretService {
 	
-
+	/**
+	 * Implementation of storing a new secret
+	 * @param userId denotes the user requesting to store a new secret
+	 * @param secret denotes the new secret to be added
+	 */
 	public UUID storeSecret(String userId, Secret secret) {
 		System.out.println("In class impl");
 		String key = userId+"#"+secret.getUUID();
-		App.userSecrets.add(key);
+		App.userSecrets.put(key,secret);
 		return secret.getUUID();
 	}
-
+	
+	/**
+	 * Implementation of reading a secret
+	 * @param userId denotes the user requesting the read operation
+	 * @param secretId denotes the id of the secret which needs to be read
+	 */
 	public Secret readSecret(String userId, UUID secretId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String key = userId+"#"+secretId;
+		if(App.userSecrets.containsKey(key))
+			return App.userSecrets.get(key);
+		else
+			return App.sharedSecrets.get(key);
 	}
-
+	
+	/**
+	 * Implementation of sharing a secret
+	 * @param userId denotes the user with whom secret is already shared/owned
+	 * @param secretId denotes the id of the secret which needs to be shared
+	 * @param  targetUserId denotes the user with whom secret is to be shared
+	 */
 	public void shareSecret(String userId, UUID secretId, String targetUserId) {
-		// TODO Auto-generated method stub
 		
+		if(!userId.equals(targetUserId)){
+			String ownerSecretKey = userId+"#"+secretId;
+			String key = targetUserId+"#"+secretId;
+			App.sharedSecrets.put(key, App.userSecrets.get(ownerSecretKey));
+		}
 	}
-
+	
+	/**
+	 * Implementation of unsharing a secret
+	 * @param userId denotes the user with whom secret is already shared/owned
+	 * @param secretId denotes the id of the secret which needs to be unshared
+	 * @param  targetUserId denotes the user with whom secret is to be unshared
+	 */
 	public void unshareSecret(String userId, UUID secretId, String targetUserId) {
-		// TODO Auto-generated method stub
 		
+		if(!userId.equals(targetUserId)){
+			String ownerSecretKey = userId+"#"+secretId;
+			if(App.userSecrets.containsKey(ownerSecretKey))
+			{
+				String key = targetUserId+"#"+secretId; 
+				App.sharedSecrets.remove(key);
+			}
+		}
 	}
-
 }
